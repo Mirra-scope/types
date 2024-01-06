a8a8df0e60e3045e43766629402f4708c044f041
zeshan shakil <zeshanshakil0@gmail.com>
2024-01-06 15:02:44 +0500

    feat: add root file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { EntityBase } from "./common/EntityBase";
+export { SuccessOutput } from "./common/CommonOutput.dto";
+
+export type { UserEntity } from "./user";
+export type { UserRegisterInput } from "./user";
+export type { UserSignInInput } from "./user";
+export type { PromoteUserToManagerInput } from "./user";
+export type { DecodeJwtTokenInput } from "./user";
+export type { AuthTokenOutput } from "./user";
+export type { DecodeJwtTokenOutput } from "./user";
+export type { UserAccountStatusEnum } from "./user";
