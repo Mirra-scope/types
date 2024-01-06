d3bc3495d5a6ee18aaa90ab735d26693b9657cdc
zeshan shakil <zeshanshakil0@gmail.com>
2024-01-06 15:19:59 +0500

    feat: remote type for export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { EntityBase } from "./common/EntityBase";
 export { SuccessOutput } from "./common/CommonOutput.dto";
 
-export type { UserEntity } from "./user";
-export type { UserRegisterInput } from "./user";
-export type { UserSignInInput } from "./user";
-export type { PromoteUserToManagerInput } from "./user";
-export type { DecodeJwtTokenInput } from "./user";
-export type { AuthTokenOutput } from "./user";
-export type { DecodeJwtTokenOutput } from "./user";
-export type { UserAccountStatusEnum } from "./user";
+export { UserEntity } from "./user";
+export { UserRegisterInput } from "./user";
+export { UserSignInInput } from "./user";
+export { PromoteUserToManagerInput } from "./user";
+export { DecodeJwtTokenInput } from "./user";
+export { AuthTokenOutput } from "./user";
+export { DecodeJwtTokenOutput } from "./user";
+export { UserAccountStatusEnum } from "./user";
--- src/user/index.ts
+++ src/user/index.ts
@@ -1,8 +1,8 @@
-export type { UserEntity } from "./User.entity";
-export type { UserRegisterInput } from "./UserInput.dto";
-export type { UserSignInInput } from "./UserInput.dto";
-export type { PromoteUserToManagerInput } from "./UserInput.dto";
-export type { DecodeJwtTokenInput } from "./UserInput.dto";
-export type { AuthTokenOutput } from "./UserOutput.dto";
-export type { DecodeJwtTokenOutput } from "./UserOutput.dto";
-export type { UserAccountStatusEnum } from "./user.enum";
+export { UserEntity } from "./User.entity";
+export { UserRegisterInput } from "./UserInput.dto";
+export { UserSignInInput } from "./UserInput.dto";
+export { PromoteUserToManagerInput } from "./UserInput.dto";
+export { DecodeJwtTokenInput } from "./UserInput.dto";
+export { AuthTokenOutput } from "./UserOutput.dto";
+export { DecodeJwtTokenOutput } from "./UserOutput.dto";
+export { UserAccountStatusEnum } from "./user.enum";
